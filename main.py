cb770777b683d7ddd54ba81bf83d61552e8a7eec
Muhammed Ali <138666455+mmali7921@users.noreply.github.com>
2025-08-07 00:38:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from pdf2image import convert_from_path
 from pyzbar.pyzbar import decode
 
-pdf_path = "test.pdf"
+if len(sys.argv) != 2:
+    print("Usage: python main.py <PDF_FILE_PATH>")
+    sys.exit(1)
+
+pdf_path = sys.argv[1]
 
 # Convert PDF pages to images
 pages = convert_from_path(pdf_path)
@@ -20,4 +25,3 @@ for page_num, page_image in enumerate(pages, start=1):
 print("\nAll extracted QR code links:")
 for link in all_links:
     print(link)
-
